e2f4ca77d8ab828db0a5cdf4143b8c3435196ec9
Priyanka Sindhu <priyankasindhu42@gmail.com>
2026-04-05 14:43:17 +0530

    fixx issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 
-import React from 'react'
-import Registration from './pages/Registration'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import UserList from './pages/UserList'
-// import Navbar from './pages/Navbar'
-import CreateShift from './pages/CreateShift'
+// import React from 'react'
+// import Registration from './pages/Registration'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import UserList from './pages/UserList'
+
+// import CreateShift from './pages/CreateShift'
 
 
-import './App.css'
-import CreateSeat from './pages/CreateSeat'
+// import './App.css'
+// import CreateSeat from './pages/CreateSeat'
 
-function App() {
+// function App() {
  
 
+//   return (
+//    <Router>
+//       <Routes>
+//         <Route path="/" element={<Registration />} />
+//         <Route path="/users" element={<UserList />} />
+//         <Route path="/create-seat" element={<CreateSeat />} />
+//         <Route path="/create-shifts" element={<CreateShift />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Registration from './pages/Registration'
+import UserList from './pages/UserList'
+import CreateShift from './pages/CreateShift'
+import CreateSeat from './pages/CreateSeat'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
   return (
-   <Router>
+    <Router>
+      <Navbar />
       <Routes>
-        
         <Route path="/" element={<Registration />} />
         <Route path="/users" element={<UserList />} />
         <Route path="/create-seat" element={<CreateSeat />} />
         <Route path="/create-shifts" element={<CreateShift />} />
-
       </Routes>
     </Router>
   )
